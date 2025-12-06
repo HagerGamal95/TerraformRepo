33dd31c9be027afc357f5306ae05c5a914d1e52e
hagergamalawad-star <hager.gamal.awad@gmail.com>
2025-12-06 18:50:51 +0200

    Add EKS cluster and update Jenkins pipeline , for our project

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.3"
 
   required_providers {
     aws = {
@@ -10,187 +10,84 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
-# Get latest Amazon Linux 2 AMI
-data "aws_ami" "amazon_linux" {
-  most_recent = true
+# --------------------------------------------------------------------
+# DATA: Get AWS Account ID
+# --------------------------------------------------------------------
+data "aws_caller_identity" "current" {}
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+# --------------------------------------------------------------------
+# VPC with 2 Public Subnets (Required by EKS)
+# --------------------------------------------------------------------
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway      = false
+  map_public_ip_on_launch = true
+
+  tags = {
+    Environment = "dev"
+    CreatedBy   = "Terraform"
   }
-
-  owners = ["137112412989"] # Amazon
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "jenkins-demo-web-sg"
-  description = "Allow HTTP and SSH"
-  vpc_id      = null # if default VPC, can be omitted in latest providers
+# --------------------------------------------------------------------
+# EKS Cluster
+# --------------------------------------------------------------------
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.30"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  cluster_endpoint_public_access = true
+
+  # ----------------------------------------------------------------
+  # IAM User Access: Give user hager full Kubernetes admin access
+  # ----------------------------------------------------------------
+  access_entries = {
+    eks-admin-user = {
+      principal_arn = "arn:aws:iam::222735209716:user/hassanhamdy"
+
+      policy_associations = {
+        eks-admin-user = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
   }
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  # ----------------------------------------------------------------
+  # Node Group: Running in one AZ (us-east-1a)
+  # ----------------------------------------------------------------
+  eks_managed_node_groups = {
+    workers = {
+      instance_types = ["t3.small"]
+      subnet_ids     = [module.vpc.public_subnets[0]]
+      min_size       = 2
+      desired_size   = 2
+      max_size       = 3
+    }
   }
 
   tags = {
-    Name = "jenkins-demo-web-sg"
+    Environment = "dev"
+    Terraform   = "true"
   }
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y httpd
-    systemctl enable httpd
-    systemctl start httpd
-
-    PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-
-    cat <<HTML > /var/www/html/index.html
-    <html>
-      <head><title>Jenkins Terraform Demo</title></head>
-      <body>
-        <h1>${var.your_name}</h1>
-        <h2>Private IP: $${PRIVATE_IP}</h2>
-      </body>
-    </html>
-    HTML
-  EOF
-
-  tags = {
-    Name = "jenkins-terraform-web"
-  }
-}
-
-output "instance_public_ip" {
-  value = aws_instance.web.public_ip
-}
-
-output "instance_private_ip" {
-  value = aws_instance.web.private_ip
-}
-
-# ------------------------------
-# EKS: Cluster + Node Group
-# ------------------------------
-
-# IAM role for EKS cluster
-resource "aws_iam_role" "eks_cluster" {
-  name = "jenkins-demo-eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
-  role       = aws_iam_role.eks_cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-# EKS cluster in default VPC public subnets
-resource "aws_eks_cluster" "this" {
-  name     = "jenkins-demo-eks"
-  role_arn = aws_iam_role.eks_cluster.arn
-  # optional: version = "1.29"  # or leave to get latest supported
-
-  vpc_config {
-    subnet_ids = data.aws_subnets.default.ids  # default VPC public subnets
-    endpoint_public_access = true
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy
-  ]
-}
-
-# IAM role for worker nodes (managed node group)
-resource "aws_iam_role" "eks_nodegroup" {
-  name = "jenkins-demo-eks-nodegroup-role"
-
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_AmazonEKSWorkerNodePolicy" {
-  role       = aws_iam_role.eks_nodegroup.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_AmazonEC2ContainerRegistryReadOnly" {
-  role       = aws_iam_role.eks_nodegroup.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_AmazonEKS_CNI_Policy" {
-  role       = aws_iam_role.eks_nodegroup.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_eks_node_group" "this" {
-  cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "jenkins-demo-eks-ng"
-  node_role_arn   = aws_iam_role.eks_nodegroup.arn
-
-  # Put worker nodes in the same (public) subnets
-  subnet_ids = data.aws_subnets.default.ids
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
-
-  instance_types = ["t3.small"]  # small & cheap for labs
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_worker_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.eks_worker_AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.eks_worker_AmazonEKS_CNI_Policy
-  ]
-}
-
-output "eks_cluster_name" {
-  value = aws_eks_cluster.this.name
-}
-
-output "eks_nodegroup_name" {
-  value = aws_eks_node_group.this.node_group_name
 }
